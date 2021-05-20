57dd6c9e54d809d3468f9525dada2f45699dc53d
celineserb <hc_serbouh@esi.dz>
2021-05-20 23:11:31 +0100

    Get Bills for each Tenant

--- Tenant/src/routes/index.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express';
-import {getRental} from '../controllers/Rental';
-
-const router = Router();
-router.get('/rental/getrental',getRental) ;/**get all tenants */
-export default router;
