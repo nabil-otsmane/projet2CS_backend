9f24546323b4728fb5176b0778a02e2abed74feb
fejzann <gs_rihani@esi.dz>
2021-05-28 19:23:12 +0100

    feat : add notification system

--- association/src/middlewares/log.ts
+++
@@ -1,9 +0,0 @@
-import { Socket } from "socket.io";
-import { ExtendedError } from "socket.io/dist/namespace";
-
-/**
- * this middleware is used to log actions of this server to the log service
- */
-export default function (_socket: Socket, next: (err?: ExtendedError | undefined) => void) {
-    next();
-}
