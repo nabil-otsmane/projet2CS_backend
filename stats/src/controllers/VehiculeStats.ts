1d7c464ff9137343489c0dfe002337b959805e2e
Ubuntu <ubuntu@ip-172-31-0-29.ec2.internal>
2021-06-03 18:14:41 +0000

    Feat Stats v2

--- stats/src/controllers/VehiculeStats.ts
+++ stats/src/controllers/VehiculeStats.ts
@@ -1,154 +1,58 @@
 import { Request, Response } from "express";
 import { getManager} from "typeorm";
-import { Vehicle } from "../entity/Vehicle";
 
-export async function VehiculesRents(_req: Request, res: Response) {
+
+
+export async function CarRentsByDay(_req: Request, res: Response) {
     const qur = await getManager()
     .createQueryBuilder()
-    .select(`vehicle."idVehicle" `)
-    .addSelect(`count(rent."idVehicle") as "TotalRents"`)
-    .from(Vehicle,"vehicle")
-    .leftJoin("Rental", "rent", `rent."idVehicle" = vehicle."idVehicle"`)
-    .groupBy(`vehicle."idVehicle"`)
+    .select(`*`)
+    .from(`( SELECT h.day,h."idVehicle" as id ,count(r."rentaldate") as rents FROM ((SELECT generate_series(
+        date_trunc('year', now()),
+        now(),
+        '1 day'
+        )::DATE AS day ) g cross join "Vehicle" v ) h 
+        LEFT JOIN "Rental" r on r."idVehicle"=h."idVehicle" and r."rentaldate"=h.day
+        group by h.day,h."idVehicle"
+        order by h.day)`,"V")
+    .where(`"V".id=:id`,{id: _req.params.id})
     .getRawMany();
     res.json(qur);
 }
 
-export async function VehiculesRentsDate(_req: Request, res: Response) {
+export async function CarRentsByMonth(_req: Request, res: Response) {
     const qur = await getManager()
     .createQueryBuilder()
-    .select(`vehicle."idVehicle" `)
-    .addSelect(`count(rent."idVehicle") as "TotalRents"`)
-    .from(Vehicle,"vehicle")
-    .leftJoin("Rental", "rent", `rent."idVehicle" = vehicle."idVehicle" AND "rentaldate" BETWEEN :start AND :end`,{start:_req.params.start, end:_req.params.end})
-    .groupBy(`vehicle."idVehicle"`)
+    .select(`*`)
+    .from(`(SELECT h.month,h."idVehicle" as id ,count(r."rentaldate") as rents FROM ((SELECT TO_CHAR(generate_series(
+        date_trunc('year', now()),
+        now(),
+        '1 month'
+        ),'Mon') as month ) g cross join "Vehicle" v ) h 
+        LEFT JOIN "Rental" r on r."idVehicle"=h."idVehicle" and TO_CHAR(r."rentaldate",'Mon')=h.month
+        group by h.month,h."idVehicle"
+        order by to_date(h.month,'Mon'))`,"V")
+    .where(`"V".id=:id`,{id: _req.params.id})
     .getRawMany();
     res.json(qur);
 }
 
-export async function VehiculesRentsYear(_req: Request, res: Response) {
+export async function CarRentsByYear(_req: Request, res: Response) {
     const qur = await getManager()
     .createQueryBuilder()
-    .select(`vehicle."idVehicle" `)
-    .addSelect(`count(rent."idVehicle") as "TotalRents"`)
-    .from(Vehicle,"vehicle")
-    .leftJoin("Rental", "rent", `rent."idVehicle" = vehicle."idVehicle" AND "rentaldate" BETWEEN :start AND :end`,{start: _req.params.year+"-01-01",end:_req.params.year+"-12-31"})
-    .groupBy(`vehicle."idVehicle"`)
+    .select(`*`)
+    .from(`(SELECT h.year,h."idVehicle" as id ,count(r."rentaldate") as rents FROM ((SELECT EXTRACT(YEAR FROM 
+        generate_series(
+        now() - INTERVAL '5 years',
+        now(),
+        '1 year'
+        )) as year ) g cross join "Vehicle" v ) h 
+        LEFT JOIN "Rental" r on r."idVehicle"=h."idVehicle" and extract(year from r."rentaldate")=h.year
+        group by h.year,h."idVehicle"
+        order by h.year)`,"V")
+    .where(`"V".id=:id`,{id: _req.params.id})
     .getRawMany();
     res.json(qur);
 }
 
-export async function VehiculesRentsYM(_req: Request, res: Response) {
-    const qur = await getManager()
-    .createQueryBuilder()
-    .select(`vehicle."idVehicle" `)
-    .addSelect(`count(rent."idVehicle") as "TotalRents"`)
-    .from(Vehicle,"vehicle")
-    .leftJoin("Rental", "rent", `rent."idVehicle" = vehicle."idVehicle" AND "rentaldate" BETWEEN :start AND :end`,{start: _req.params.year+"-"+_req.params.month+"-01",end:_req.params.year+"-"+(parseInt(_req.params.month)+1)+"-01"})
-    .groupBy(`vehicle."idVehicle"`)
-    .getRawMany();
-    res.json(qur);
-}
 
-export async function VehiculesRentsYMD(_req: Request, res: Response) {
-    const qur = await getManager()
-    .createQueryBuilder()
-    .select(`vehicle."idVehicle" `)
-    .addSelect(`count(rent."idVehicle") as "TotalRents"`)
-    .from(Vehicle,"vehicle")
-    .leftJoin("Rental", "rent", `rent."idVehicle" = vehicle."idVehicle" AND "rentaldate"=:date`,{date: _req.params.year+"-"+_req.params.month+"-"+_req.params.day})
-    .groupBy(`vehicle."idVehicle"`)
-    .getRawMany();
-    res.json(qur);
-}
-
-export async function VehiculeRents(_req: Request, res: Response) {
-    const qur = await getManager()
-    .createQueryBuilder()
-    .select(`vehicle."idVehicle" `)
-    .addSelect(`count(rent."idVehicle") as "TotalRents"`)
-    .from(Vehicle,"vehicle")
-    .leftJoin("Rental", "rent", `rent."idVehicle" = vehicle."idVehicle"`)
-    .where(`vehicle."idVehicle"=:id`,{id: _req.params.id})
-    .groupBy(`vehicle."idVehicle"`)
-    .getRawOne();
-    res.json(qur);
-}
-
-export async function VehiculeRentsDate(_req: Request, res: Response) {
-    const qur = await getManager()
-    .createQueryBuilder()
-    .select(`vehicle."idVehicle" `)
-    .addSelect(`count(rent."idVehicle") as "TotalRents"`)
-    .from(Vehicle,"vehicle")
-    .leftJoin("Rental", "rent", `rent."idVehicle" = vehicle."idVehicle" AND "rentaldate" BETWEEN :start AND :end`,{start:_req.params.start, end:_req.params.end})
-    .where(`vehicle."idVehicle"=:id`,{id: _req.params.id})
-    .groupBy(`vehicle."idVehicle"`)
-    .getRawOne();
-    res.json(qur);
-}
-
-export async function VehiculeRentsYear(_req: Request, res: Response) {
-    const qur = await getManager()
-    .createQueryBuilder()
-    .select(`vehicle."idVehicle" `)
-    .addSelect(`count(rent."idVehicle") as "TotalRents"`)
-    .from(Vehicle,"vehicle")
-    .leftJoin("Rental", "rent", `rent."idVehicle" = vehicle."idVehicle" AND "rentaldate" BETWEEN :start AND :end`,{start: _req.params.year+"-01-01",end:_req.params.year+"-12-31"})
-    .where(`vehicle."idVehicle"=:id`,{id: _req.params.id})
-    .groupBy(`vehicle."idVehicle"`)
-    .getRawOne();
-    res.json(qur);
-}
-
-export async function VehiculeRentsYM(_req: Request, res: Response) {
-    const qur = await getManager()
-    .createQueryBuilder()
-    .select(`vehicle."idVehicle" `)
-    .addSelect(`count(rent."idVehicle") as "TotalRents"`)
-    .from(Vehicle,"vehicle")
-    .leftJoin("Rental", "rent", `rent."idVehicle" = vehicle."idVehicle" AND "rentaldate" BETWEEN :start AND :end`,{start: _req.params.year+"-"+_req.params.month+"-01",end:_req.params.year+"-"+(parseInt(_req.params.month)+1)+"-01"})
-    .where(`vehicle."idVehicle"=:id`,{id: _req.params.id})
-    .groupBy(`vehicle."idVehicle"`)
-    .getRawOne();
-    res.json(qur);
-}
-
-export async function VehiculeRentsYMD(_req: Request, res: Response) {
-    const qur = await getManager()
-    .createQueryBuilder()
-    .select(`vehicle."idVehicle" `)
-    .addSelect(`count(rent."idVehicle") as "TotalRents"`)
-    .from(Vehicle,"vehicle")
-    .leftJoin("Rental", "rent", `rent."idVehicle" = vehicle."idVehicle" AND "rentaldate"=:date`,{date: _req.params.year+"-"+_req.params.month+"-"+_req.params.day})
-    .where(`vehicle."idVehicle"=:id`,{id: _req.params.id})
-    .groupBy(`vehicle."idVehicle"`)
-    .getRawOne();
-    res.json(qur);
-}
-
-
-export async function signalEnlev(_req: Request, res: Response) {
-    const qur = await getManager()
-    .createQueryBuilder()
-    .select(`vehicle."idVehicle" `)
-    .addSelect(`count(signal."idVehicle") as "TotalSignals"`)
-    .from(Vehicle,"vehicle")
-    .leftJoin("Signal", "signal", `signal."idVehicle" = vehicle."idVehicle" AND "signalType"=:type`,{type:"theft"})
-    .groupBy(`vehicle."idVehicle"`)
-    .getRawMany();
-    res.json(qur);
-}
-
-export async function signalEnlevById(_req: Request, res: Response) {
-    const qur = await getManager()
-    .createQueryBuilder()
-    .select(`vehicle."idVehicle" `)
-    .addSelect(`count(signal."idVehicle") as "TotalSignals"`)
-    .from(Vehicle,"vehicle")
-    .leftJoin("Signal", "signal", `signal."idVehicle" = vehicle."idVehicle" AND "signalType"=:type`,{type:"theft"})
-    .where(`vehicle."idVehicle"=:id`,{id: _req.params.id})
-    .groupBy(`vehicle."idVehicle"`)
-    .getRawOne();
-    res.json(qur);
-}
