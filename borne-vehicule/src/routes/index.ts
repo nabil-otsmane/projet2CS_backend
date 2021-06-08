a2d7947d8e58bc01a0efe1825e8752b1f5c2feb0
BillelBenoudjit <hb_benoudjit@esi.dz>
2021-06-08 16:22:18 +0100

    Add get Vehicles by idBorne route

--- borne-vehicule/src/routes/index.ts
+++ borne-vehicule/src/routes/index.ts
@@ -1,25 +1,33 @@
-import { Router } from 'express';
-import { getBorne, getBornes, addBorne, updateBorne, deleteBorne } from '../controllers/borne'
-import { getVehicule, getVehicules, addVehicule, updateVehicule, deleteVehicule } from '../controllers/vehicule'
+import { Router } from "express";
+import {
+  getBorne,
+  getBornes,
+  addBorne,
+  updateBorne,
+  deleteBorne,
+} from "../controllers/borne";
+import {
+  getVehicule,
+  getVehicules,
+  addVehicule,
+  updateVehicule,
+  deleteVehicule,
+  getVehiculesByBorneId,
+} from "../controllers/vehicule";
 
 const router = Router();
 
+router.post("/bornes", addBorne);
+router.get("/bornes", getBornes);
+router.get("/bornes/:idBorne", getBorne);
+router.put("/bornes/:idBorne", updateBorne);
+router.delete("/bornes/:idBorne", deleteBorne);
 
-router.post('/bornes', addBorne)
-router.get('/bornes', getBornes)
-router.get('/bornes/:idBorne', getBorne)
-router.put('/bornes/:idBorne', updateBorne)
-router.delete('/bornes/:idBorne', deleteBorne)
-
-
-
-router.post('/vehicules', addVehicule)
-router.get('/vehicules', getVehicules)
-router.get('/vehicules/:idVehicule', getVehicule)
-router.put('/vehicules/:idVehicule', updateVehicule)
-router.delete('/vehicules/:idVehicule', deleteVehicule)
-
-
-
+router.post("/vehicules", addVehicule);
+router.get("/vehicules", getVehicules);
+router.get("/vehicules/:idBorne", getVehiculesByBorneId);
+router.get("/vehicules/:idVehicule", getVehicule);
+router.put("/vehicules/:idVehicule", updateVehicule);
+router.delete("/vehicules/:idVehicule", deleteVehicule);
 
 export default router;
