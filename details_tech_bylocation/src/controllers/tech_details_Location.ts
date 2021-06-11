e8a88ba130ca8fa1f3ac4f5467d5853336cfe655
Khadidja Habelhames <hk_hab_el_hames@esi.dz>
2021-06-11 11:06:25 +0100

    added CRUD operations for pannes service

--- details_tech_bylocation/src/controllers/tech_details_Location.ts
+++ details_tech_bylocation/src/controllers/tech_details_Location.ts
@@ -7,13 +7,12 @@ export const get = (_req: Request, res: Response) => {
     res.end("This service is up and running !");
 }
 
-
 export const add_techDetails_ByLocation = async (req: Request, res: Response) => {
 
     const {idRental,engineTemp,fuelLevel,oilPressure,batteryCharge,brakeFuild,speed,kilos} = req.body;
 
       const state= await VehicleState.findOne({idRental:idRental})
-        if(!state){
+        if(!state) {
             const vehiclestate = VehicleState.create({
                 idRental: req.body.idRental,
                 engineTemp: req.body.engineTemp,
@@ -27,16 +26,16 @@ export const add_techDetails_ByLocation = async (req: Request, res: Response) =>
             await vehiclestate.save()
             res.status(200).send(vehiclestate)
         }
-        else{
-            state.engineTemp = engineTemp 
-            state.fuelLevel = fuelLevel 
-            state.oilPressure=oilPressure
-            state.batteryCharge=batteryCharge
-            state.brakeFuild=brakeFuild
-            state.speed=speed
-            state.kilos=kilos
-            await state.save()
-            res.status(200).send(state)
+        else {
+                state.engineTemp = engineTemp 
+                state.fuelLevel = fuelLevel 
+                state.oilPressure=oilPressure
+                state.batteryCharge=batteryCharge
+                state.brakeFuild=brakeFuild
+                state.speed=speed
+                state.kilos=kilos
+                await state.save()
+                res.status(200).send(state)
         }
 }
     
