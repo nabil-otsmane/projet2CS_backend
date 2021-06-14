10254815f71889746fc0b88b177bf42c647cee98
Lydia <hl_brinis@esi.dz>
2021-06-14 15:56:01 +0200

    adding promo code index

---
+++ PromoCodeService/src/index.ts
@@ -0,0 +1,28 @@
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+
+import * as express from 'express';
+import { json } from "express";
+import * as cors from 'cors';
+import * as morgan from 'morgan';
+import Router from './routes'
+
+const app = express()
+
+
+app.use(json())
+app.use(cors())
+app.use(morgan("dev"))
+
+app.use('/promocode',Router)
+var s;
+createConnection().then(async _connection => {   
+    s = app.listen(8080, () => {
+        console.log("server started.")
+    })
+    
+
+}).catch(error => console.log(error));
+
+export const server = s;
+export default app;
