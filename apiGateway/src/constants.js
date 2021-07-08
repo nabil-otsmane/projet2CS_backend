a228cfe4620dc7f815401f8bb9f08d51d33beef2
nabil-otsmane <hn_otsmane@esi.dz>
2021-07-08 22:53:25 +0100

    added hosts to apigateway

--- apiGateway/src/constants.js
+++ apiGateway/src/constants.js
@@ -1,8 +1,8 @@
 module.exports = {
-    auth: "",
-    user: "",
-    association: "",
-    log: "",
-    stats: "",
-    borne_vehicule: "",
+    auth: "http://54.37.87.85:7000/auth/",
+    user: "https://cryptic-dawn-38491.herokuapp.com/",
+    association: "https://protected-depths-44465.herokuapp.com/",
+    log: "https://log-project2cs.herokuapp.com/",
+    stats: "https://utilisateurs.herokuapp.com/",
+    borne_vehicule: "http://54.37.87.85:7000/bornes/",
 }
