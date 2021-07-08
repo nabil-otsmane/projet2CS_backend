54b4ae974d3916f5e005a664ea805666973c49ab
Zineddine <root@ns3107429.ip-54-37-87.eu>
2021-07-08 17:56:43 +0000

    local change

--- association/src/index.ts
+++ association/src/index.ts
@@ -1,8 +1,6 @@
 import * as dotenv from 'dotenv'
-dotenv.config()
-import { createServer } from 'http';
 
-import "reflect-metadata";
+import { createServer } from 'http';
 import { Connection, createConnection } from "typeorm";
 import * as express from 'express';
 import { createClient } from 'redis'
@@ -10,7 +8,7 @@ import * as cors from 'cors';
 import * as morgan from 'morgan';
 import { Server, ServerOptions } from 'socket.io'
 import initConnection from './routes'
-
+dotenv.config()
 
 const app = express()
 
