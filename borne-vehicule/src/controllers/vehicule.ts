913d18d00cae4d448846fe7668d9d6035269b996
Zineddine <root@ns3107429.ip-54-37-87.eu>
2021-07-09 12:06:38 +0000

    restore changes

--- borne-vehicule/src/controllers/vehicule.ts
+++ borne-vehicule/src/controllers/vehicule.ts
@@ -1,8 +1,11 @@
 import { Request, Response } from "express";
-import { getRepository } from "typeorm";
 import { getConnection } from "typeorm";
-
 import { Vehicule } from "../entity/Vehicule";
+import { Borne } from "../entity/Borne";
+import { Rental } from "../entity/Rental";
+import { VehicleTracking } from "../entity/VehicleTracking";
+import { VehiclePosition } from "../entity/VehiclePosition";
+
 
 export const getVehicule = (req: Request, res: Response) => {
   Vehicule.findOne({ idVehicle: parseInt(req.params.idVehicule) })
@@ -139,4 +142,95 @@ export const deleteVehicule = async (req: Request, res: Response) => {
         message: "Erreur Serveur",
       });
     });
+};
+
+export const updateInfoVehicle = async (req: Request, res: Response) => {
+  const idVehicle = parseInt(req.params.id);
+  const { idBorne } = req.body;
+  let borne: Borne
+  let idBorneVar: number
+  let rental
+  let tracking
+
+  try {
+    // Update Borne Vehicle
+    const vehicle = await Vehicule.findOneOrFail({ idVehicle })
+    console.log("Here")
+    if (idBorne == null) {
+      throw "Id Borne Invalide"
+    } else {
+      // ASK ASMA IF N'incrémenté Le nombre de place f l borne la9dima
+      idBorneVar = (vehicle.idBorne as unknown) as number;
+      // Update Number of occupied places in the previous borne
+      borne = await Borne.findOneOrFail({ idBorne: idBorneVar });
+      borne.nbOccupiedPlaces--;
+      await borne.save();
+
+      // Update Vehicle Borne
+      vehicle.idBorne = idBorne
+
+
+      // Update Number of occupied places in the newest borne
+      borne = await Borne.findOneOrFail({ idBorne })
+      borne.nbOccupiedPlaces++;
+      await borne.save();
+    }
+
+    // Update the Vehicle Position
+    // Tell Asma That Rental Ou Vehicle Position fihom des données Marahoumsh Cohérente
+
+    // 01- Get The last rental relative to that vehicule
+    rental = await Rental.find({ idVehicle })
+    const latestOne = new Date(Math.max.apply(null, rental.map(function (e) {
+      return new Date(e.rentaldate);
+    })));
+    rental = await Rental.findOneOrFail({ idVehicle, rentaldate: latestOne })
+
+    // 02- Access to Table Tracking & Get The last inserted tracking of that vehicle
+    const position = await VehiclePosition.findOneOrFail({ idRental: rental.idRental })
+    tracking = await VehicleTracking.find({ idPosition: position.idPosition })
+    const lastTrack = Math.max.apply(null, tracking.map(function (e) {
+      return e.idTrack;
+    }))
+    tracking = await VehicleTracking.findOneOrFail({ idTrack: lastTrack })
+
+    // 03-use the tracking info( longitude & latitude) to update the position of the vehicle in vehicle table (longitude and latitude)
+    vehicle.latitude = tracking.latitude
+    vehicle.longitude = tracking.longitude
+    await vehicle.save();
+
+    return res.json(vehicle);
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(500)
+      .json({ error: "Something went wrong while updating vehicle borne..." });
+  }
+};
+
+export const getRentalVehicle = async (req: Request, res: Response) => {
+  const idVehicle = parseInt(req.params.id);
+  let rental
+  let tracking
+  try {
+    rental = await Rental.find({ idVehicle })
+    const latestOne = new Date(Math.max.apply(null, rental.map(function (e) {
+      return new Date(e.rentaldate);
+    })));
+    rental = await Rental.findOneOrFail({ idVehicle, rentaldate: latestOne })
+
+    const position = await VehiclePosition.findOneOrFail({ idRental: rental.idRental })
+    tracking = await VehicleTracking.find({ idPosition: position.idPosition })
+    const lastTrack = Math.max.apply(null, tracking.map(function (e) {
+      return e.idTrack;
+    }))
+    tracking = await VehicleTracking.findOneOrFail({ idTrack: lastTrack })
+
+    return res.json(tracking);
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(500)
+      .json({ error: "Something went wrong while geting rental vehicle..." });
+  }
 };
