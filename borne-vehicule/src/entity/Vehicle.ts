380dedcd1894a016ce1d9d2d0109ff79a2bd7a2c
BRINIS-Lydia <hl_brinis@esi.dz>
2021-07-09 12:58:29 +0100

    added entities for borne-vehicule

---
+++ borne-vehicule/src/entity/Vehicle.ts
@@ -0,0 +1,56 @@
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
+import { IsEnum } from "class-validator";
+
+@Entity("Vehicle")
+export class Vehicle extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  idVehicle: number;
+
+  @Column()
+  unitPricePerHour: number;
+
+  @Column()
+  unitPricePerDay: number;
+
+  @Column()
+  vehicleType: String;
+
+  @Column()
+  vehiclebrand: String;
+
+  @Column()
+  vehiclemodel: String;
+
+  @Column()
+  fuelType: String;
+
+  @Column()
+  registrationNumber: String;
+
+  @Column()
+  vehicleColor: String;
+
+  @Column()
+  idBorne: Number;
+
+  @Column({ type: "float" })
+  longitude: Number;
+
+  @Column({ type: "float" })
+  latitude: Number;
+
+  @Column()
+  image: String;
+
+
+  @Column({
+    type: "enum",
+    enum: ["available", "unavailable"],
+    default: "available",
+  })
+  @IsEnum(["available", "unavailable"])
+  availibility: String;
+
+  @Column()
+  chassisNumber:string;
+}
