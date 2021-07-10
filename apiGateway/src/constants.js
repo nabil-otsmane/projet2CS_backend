9d522ed059cb04ecc879396cf70e067c9d6e453f
nabil-otsmane <hn_otsmane@esi.dz>
2021-07-10 17:44:51 +0100

    fix: changed user service location

--- apiGateway/src/constants.js
+++ apiGateway/src/constants.js
@@ -1,6 +1,6 @@
 module.exports = {
     auth: "http://54.37.87.85:7000/auth/",
-    user: "https://cryptic-dawn-38491.herokuapp.com/",
+    user: "https://user-project2cs.herokuapp.com/",
     association: "https://protected-depths-44465.herokuapp.com/",
     log: "https://log-project2cs.herokuapp.com/",
     stats: "https://utilisateurs.herokuapp.com/",
