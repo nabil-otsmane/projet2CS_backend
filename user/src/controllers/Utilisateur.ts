c022df25f8d5143c0ee827c86d9e70863bf139ff
nabil-otsmane <hn_otsmane@esi.dz>
2021-07-11 00:23:20 +0100

    fix: add userType to user update

--- user/src/controllers/Utilisateur.ts
+++ user/src/controllers/Utilisateur.ts
@@ -57,7 +57,8 @@ export const updateUtilisateur = async (req: Request, res: Response) => {
         firstName: req.body.prenom,
         phoneNumber: req.body.numeroTelephone,
         address: req.body.address,
-        userName: req.body.userName
+        userName: req.body.userName,
+        userType: req.body.userType
     })
     .then(utilisateur => {
         res.send(utilisateur);
